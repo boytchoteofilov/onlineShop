e0f9f694d8f3d4f472028159a278dca0f9a3cbb9
Boytcho Teofilov <boytchoteofilov@users.noreply.github.com>
2020-08-10 12:00:35 +0100

    Added models and repositories

---
+++ onlineShop.WEB/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlineShop.WEB.Data.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; }
+
+        IEnumerable<Drink> PreferredDrinks { get; }
+
+        Drink GetDrinkById(int drinkId);
+    }
+}
---
+++ onlineShop.WEB/Data/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using onlineShop.WEB.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlineShop.WEB.Data.Mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories {
+            get
+            {
+                return new List<Category>
+                     {
+                         new Category { CategoryName = "Alcoholic", Description = "All alcoholic drinks" },
+                         new Category { CategoryName = "Non-alcoholic", Description = "All non-alcoholic drinks" }
+                     };
+            }
+        }
+    }
+}
